9473fb9abe4f0064279422465117817c630c116e
puechua <puechua@gmail.com>
2023-10-10 17:37:23 +0200

    Dodaj pliki projektów.

---
+++ CastType/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 10;
+            byte myByte = (byte)myInt;
+            double myDouble = (double)myByte;
+            bool myBool = (bool)myDouble; // nie da sie - double na bool
+            string myString = "false";
+            myBool = (bool)myString; // nie da sie - string na bool
+            myString = (string)myInt; // nie da sie - int na string
+            myString = myInt.ToString();
+            myBool = (bool)myByte; // nie da sie - byte na bool
+            myByte = (byte)myBool; // nie da sie - bool na byte
+            short myShort = (short)myInt;
+            char myChar = 'x';
+            myString = (string)myChar; // nie da sie - char na string
+            long myLong = (long)myInt;
+            decimal myDecimal = (decimal)myLong;
+            myString = myString + myInt + myByte + myDouble + myChar;
+
+
+        }
+    }
+}
---
+++ CastType/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("CastType")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CastType")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("4b37b775-77ed-48d0-92ee-f5a9404b8aa0")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
